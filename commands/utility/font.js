cd528bd782c17cc083739e71465e035e13346307
BrendenHaskins <brendenshaskins@gmail.com>
2024-06-27 02:11:07 -0700

    change message

--- commands/utility/font.js
+++ commands/utility/font.js
@@ -5,6 +5,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('font')
 		.setDescription('Changes the font of a message')
+
 		.addStringOption(option =>
 			option.setName('font')
 				.setDescription('font desired')
@@ -12,10 +13,18 @@ module.exports = {
 				.addChoices(
 					{name: 'Math Bold Script', value: 'mbs'},
 					{name: 'Braille', value: 'brl'}
-				)),
+				))
+
+		.addStringOption(option =>
+			option.setName('message')
+				.setDescription('message to alter')
+				.setRequired(true)
+		),
+		
 	async execute(interaction) {
 		const font = interaction.options.getString('font');
-		const response = fontUtil.changeFont(interaction.content, font);
+		const message = interaction.option.getString('message');
+		const response = fontUtil.changeFont(message, font);
 		
 
 

c1f595431c89f1b24232c4497a7d49cda445e919
BrendenHaskins <brendenshaskins@gmail.com>
2024-06-26 21:46:05 -0700

    deployment mode: global

--- deploy-commands.js
+++ deploy-commands.js
@@ -40,7 +40,8 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+            //MODE: GLOBAL
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 

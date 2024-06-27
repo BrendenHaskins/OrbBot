10a9b01664f296785d44a56f6356ee6f9400ede7
BrendenHaskins <brendenshaskins@gmail.com>
2024-06-26 22:28:40 -0700

    revert to guild mode for development

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,7 +5,7 @@
  */
 
 const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
+const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -41,7 +41,7 @@ const rest = new REST().setToken(token);
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
             //MODE: GLOBAL
-			Routes.applicationCommands(clientId),
+			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
 

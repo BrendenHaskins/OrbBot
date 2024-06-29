704fb515b718983c9dde7fb519e8912e8fc1e3cb
BrendenHaskins <brendenshaskins@gmail.com>
2024-06-28 18:30:38 -0700

    implement braille font utility using surrogate pair

--- commands/utility/helpers/fontUtilities.js
+++ commands/utility/helpers/fontUtilities.js
@@ -7,6 +7,10 @@ const mathBoldScriptUnicode =  [ 0x1D4EA, 0x1D4EB, 0x1D4EC, 0x1D4ED, 0x1D4EE, 0x
     0x1D4F4, 0x1D4F5, 0x1D4F6, 0x1D4F7, 0x1D4F8, 0x1D4F9, 0x1D4FA, 0x1D4FB, 0x1D4FC, 0x1D4FD, 0x1D4FE, 0x1D4FF, 0x1D500,
     0x1D501, 0x1D502, 0x1D503  ]
 
+const braille = [0x2801, 0x2803, 0x2809, 0x2819, 0x2811, 0x280B, 0x281B, 0x2813, 0x280A, 0x281A, 0x2805, 0x2807, 0x280D,
+     0x281D, 0x2815, 0x280F, 0x281F, 0x2817, 0x280E, 0x281E, 0x2825, 0x2827, 0x283A, 0x282D, 0x283D, 0x2835];
+
+
 function changeFont(body, font) {
     let characters = Array.from(body);
 
@@ -24,6 +28,9 @@ function changeFont(body, font) {
                 switch (font) {
                     case 'mbs':
                         output += parseUnicode(mathBoldScriptUnicode[code]);
+                        break;
+                    case 'brl':
+                        output += parseUnicode(braille[code]);
                         break;
                 }
             }
